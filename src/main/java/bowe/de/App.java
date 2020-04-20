ae4c3040e050d5f518c60432572c2e2ea206380d
Bastian Bowe <bastian.bowe@otto.de>
2020-04-20 20:41:42 +0200

    Demo

--- src/main/java/bowe/de/App.java
+++ src/main/java/bowe/de/App.java
@@ -9,5 +9,14 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("Hello Worlds!");
+        boolean stepInto = false;
+        if (stepInto) {
+            System.out.println("Hello Worlds!");
+            System.out.println("Hello Worlds!");
+            System.out.println("Hello Worlds!");
+            System.out.println("Hello Worlds!");
+            System.out.println("Hello Worlds!");
+            System.out.println("Hello Worlds!");
+        }
     }
 }
